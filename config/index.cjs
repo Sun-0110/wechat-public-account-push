2556f8d7300a62a6036efe1a30bbaa5093244891
Sun-0110 <102288301+Sun-0110@users.noreply.github.com>
2022-11-12 20:22:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '朋友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'obImO50wlikto0Xt199I_r8Eb28I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'FUltKSlXLzgKbELewtYx2pNoUYQwZ85wAaDUOk8yZH0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
